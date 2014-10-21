6d8d9063dd069ed423e976565e4a6ee0656f80eb
Marcel Molitor <github-mm@dont-spam-me.de>
2014-10-21 16:28:25 +0200

    initial commit

---
+++ Spielwiese/src/de/hof_university/iisys/pp_vinf12/lucene_clustering/spielwiese/Test.java
@@ -0,0 +1,10 @@
+package de.hof_university.iisys.pp_vinf12.lucene_clustering.spielwiese;
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+	}
+
+}
