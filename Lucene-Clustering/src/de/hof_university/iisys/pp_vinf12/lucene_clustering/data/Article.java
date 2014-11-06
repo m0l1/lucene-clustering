6b0c274e9942a7cc11bc26aafcc99b150dc81d70
vi-ckoepf <vi-ckoepf@fa-ls102-02.hof-university.de>
2014-11-06 14:31:33 +0100

    Datenklassen erstellt

---
+++ Lucene-Clustering/src/de/hof_university/iisys/pp_vinf12/lucene_clustering/data/Article.java
@@ -0,0 +1,71 @@
+package de.hof_university.iisys.pp_vinf12.lucene_clustering.data;
+
+import java.util.GregorianCalendar;
+
+//@author ckoepf
+//change 06.11.2014
+
+public class Article {
+	
+	//TODO Kommentar hinzufügen
+
+	private String title;
+	private String description;
+	private String source;
+	private String language;
+	private String text;
+	private String logo;
+	private String link;
+	private GregorianCalendar date;
+	
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getSource() {
+		return source;
+	}
+	public void setSource(String source) {
+		this.source = source;
+	}
+	public String getLanguage() {
+		return language;
+	}
+	public void setLanguage(String language) {
+		this.language = language;
+	}
+	public String getText() {
+		return text;
+	}
+	public void setText(String text) {
+		this.text = text;
+	}
+	public String getLogo() {
+		return logo;
+	}
+	public void setLogo(String logo) {
+		this.logo = logo;
+	}
+	public String getLink() {
+		return link;
+	}
+	public void setLink(String link) {
+		this.link = link;
+	}
+	public GregorianCalendar getDate() {
+		return date;
+	}
+	public void setDate(GregorianCalendar date) {
+		this.date = date;
+	}
+
+	
+}
---
+++ Lucene-Clustering/src/de/hof_university/iisys/pp_vinf12/lucene_clustering/data/Cluster.java
@@ -0,0 +1,43 @@
+package de.hof_university.iisys.pp_vinf12.lucene_clustering.data;
+
+import java.util.GregorianCalendar;
+import java.util.List;
+
+//@author ckoepf
+//change 06.11.2014
+
+public class Cluster {
+	
+	private Article topArticle;
+	private List<Article> articles;
+	private GregorianCalendar created;
+	private GregorianCalendar lastChange;
+	//TODO Variable Vektor-Space-Model?
+	
+	public Article getTopArticle() {
+		return topArticle;
+	}
+	public void setTopArticle(Article topArticle) {
+		this.topArticle = topArticle;
+	}
+	public List<Article> getArticles() {
+		return articles;
+	}
+	public void setArticles(List<Article> articles) {
+		this.articles = articles;
+	}
+	public GregorianCalendar getCreated() {
+		return created;
+	}
+	public void setCreated(GregorianCalendar created) {
+		this.created = created;
+	}
+	public GregorianCalendar getLastChange() {
+		return lastChange;
+	}
+	public void setLastChange(GregorianCalendar lastChange) {
+		this.lastChange = lastChange;
+	}
+
+	
+}
