394bd36de3616fd391017ad5baa97212eac9f9ba
vi-mlouis <vi-mlouis@fa-ls104-01.hof-university.de>
2014-11-20 15:29:14 +0100

    XML-Parser parst jetzt in die Article-Klasse

--- Spielwiese/src/de/hof_university/iisys/pp_vinf12/lucene_clustering/xml/XMLExample.java
+++ Spielwiese/src/de/hof_university/iisys/pp_vinf12/lucene_clustering/xml/XMLExample.java
@@ -1,10 +1,20 @@
 package de.hof_university.iisys.pp_vinf12.lucene_clustering.xml;
 import java.util.Map;
 
+import de.hof_university.iisys.pp_vinf12.lucene_clustering.data.Article;
+
 public class XMLExample {
 	public static void main(String[] args) {
 		XMLParser xml = new XMLParser();
-		Map<String, String> mapTest = xml.parse("C:/Users/Maxim_000/Schule/Semester 5/Eclipse/Praktikum Programmieren/rssfiles/germany/de/travel/ZEITONLINEReisen/y2013/m9/d30/RSS-2083299493.xml");
-		System.out.println(mapTest.values());
+		Article testArticle = xml.parse("H:/RSS622323681.xml");
+		System.out.println("Description: " + testArticle.getDescription());
+		System.out.println("Language: " + testArticle.getLanguage());
+		System.out.println("Link: " + testArticle.getLink());
+		System.out.println("Logo: " + testArticle.getLogo());
+		System.out.println("Source: " + testArticle.getSource());
+		System.out.println("Text: " + testArticle.getText());
+		System.out.println("Title: " + testArticle.getTitle());
+		System.out.println("Date: " + testArticle.getDate().getTime());
+		
 	}
 }
