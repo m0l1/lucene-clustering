b1bd982f17a190e8fd284ee3b2dc9425b8825fb7
vi-mlouis <vi-mlouis@fb-ls006-20.hof-university.de>
2014-11-21 09:15:39 +0100

    Erster Versuch der Einbindung einer List<Cluster> in html-Seiten.

--- Spielwiese/src/de/hof_university/iisys/pp_vinf12/lucene_clustering/gui/ArticleManagedBean.java
+++ Spielwiese/src/de/hof_university/iisys/pp_vinf12/lucene_clustering/gui/ArticleManagedBean.java
@@ -1,23 +1,39 @@
 package de.hof_university.iisys.pp_vinf12.lucene_clustering.gui;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
+import de.hof_university.iisys.pp_vinf12.lucene_clustering.data.Article;
 import de.hof_university.iisys.pp_vinf12.lucene_clustering.data.Cluster;
  
 @ManagedBean
 @SessionScoped
 public class ArticleManagedBean {
-    private Cluster cluster = new Cluster();
- 
+    private List<Cluster> clusterList;
+    
+    // http://stackoverflow.com/questions/9186364/how-do-i-display-a-list-of-items-from-a-bean-onto-a-jsf-webpage
+    
     public ArticleManagedBean(){
+    	
+    	List<Cluster> clList = new ArrayList<Cluster>();
+    	Cluster cl = new Cluster();
+    	Article art = new Article();
+    	art.setTitle("TestArtikelTitel");
+    	cl.setTopArticle(art);
+    	clList.add(cl);
+    	
     }
 
-	public Cluster getCluster() {
-		return cluster;
+	public List<Cluster> getClusterList() {
+		return clusterList;
 	}
 
-	public void setCluster(Cluster cluster) {
-		this.cluster = cluster;
+	public void setClusterList(List<Cluster> clusterList) {
+		this.clusterList = clusterList;
 	}
+
 
 }
