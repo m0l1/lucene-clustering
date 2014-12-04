0b01581eb08e40ab848b5f6a5992b04b0e1530b5
dominikockl <dominikockl@wstud2-5c969d745379.hof-university.de>
2014-12-04 11:27:27 +0100

    Klasse zum testen der Cluster.. Artikel anlegen und in eine Liste hinzufügen und Cluster anschließend befüllen

---
+++ Spielwiese/src/de/hof_university/iisys/pp_vinf12/lucene_clustering/gui/DatenTest.java
@@ -0,0 +1,39 @@
+package de.hof_university.iisys.pp_vinf12.lucene_clustering.gui;
+
+import java.util.*;
+import de.hof_university.iisys.pp_vinf12.lucene_clustering.data.*;
+import de.hof_university.iisys.pp_vinf12.lucene_clustering.xml.XMLParser;
+
+//@author mlouis doeckl
+
+public class DatenTest {
+	public static List<Cluster> getData(){
+		//Clusterliste und Cluster vorbereiten
+		List<Cluster> clusterList  = new ArrayList<Cluster>();
+		Cluster c1 = new Cluster();
+		Cluster c2 = new Cluster();	
+		//xml parser anlegen
+		XMLParser parser = new XMLParser();
+		// Artikel anlegen und in eine Liste hinzufügen
+		Article a1 = parser.parse("");
+		Article a2 = parser.parse("");
+		Article a3 = parser.parse("");
+		Article a4 = parser.parse("");
+		List<Article> articles1 = new ArrayList<Article>();
+		List<Article> articles2 = new ArrayList<Article>();
+		articles1.add(a1);
+		articles1.add(a2);
+		articles2.add(a3);
+		articles2.add(a4);
+		//Cluster befüllen
+		c1.setTopArticle(parser.parse(""));
+		c2.setTopArticle(parser.parse(""));
+		c1.setArticles(articles1);
+		c2.setArticles(articles2);
+		//Cluster der Liste hinzufügen
+		clusterList.add(c1);
+		clusterList.add(c2);
+		
+		return clusterList;
+	}
+}
