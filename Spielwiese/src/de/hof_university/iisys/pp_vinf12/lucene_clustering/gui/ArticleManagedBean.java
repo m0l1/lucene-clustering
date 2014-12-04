5b28fe2d8e71ccb44eb8c02281c397fb1248cf30
Max <Max@192.168.189.50>
2014-12-04 14:09:10 +0100

    Testcommit weil sonst nichts mehr geht ...

--- Spielwiese/src/de/hof_university/iisys/pp_vinf12/lucene_clustering/gui/ArticleManagedBean.java
+++ Spielwiese/src/de/hof_university/iisys/pp_vinf12/lucene_clustering/gui/ArticleManagedBean.java
@@ -1,4 +1,6 @@
 package de.hof_university.iisys.pp_vinf12.lucene_clustering.gui;
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
@@ -7,16 +9,16 @@ import de.hof_university.iisys.pp_vinf12.lucene_clustering.data.Cluster;
 @ManagedBean
 @SessionScoped
 public class ArticleManagedBean {
-    private Cluster cluster = new Cluster();
+    private List<Cluster> cluster;
  
     public ArticleManagedBean(){
     }
 
-	public Cluster getCluster() {
+	public List<Cluster> getCluster() {
 		return cluster;
 	}
 
-	public void setCluster(Cluster cluster) {
+	public void setCluster(List<Cluster> cluster) {
 		this.cluster = cluster;
 	}
 
