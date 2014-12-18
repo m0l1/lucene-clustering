0fd97d2888cd48e4f05b3006feec0044ba1af2ca
Max <Max@192.168.191.57>
2014-12-18 12:11:42 +0100

    Richtig saubere Lösung für die Bean, ClusterWrapper ist damit unnötig und wurde gelöscht. Relative Links für CSS und Bild gesetzt. Ich bin so glücklich! <3
    TODO: Suche

--- Spielwiese/src/de/hof_university/iisys/pp_vinf12/lucene_clustering/gui/DatenTest.java
+++ Spielwiese/src/de/hof_university/iisys/pp_vinf12/lucene_clustering/gui/DatenTest.java
@@ -36,4 +36,34 @@ public class DatenTest {
 		
 		return clusterList;
 	}
+	
+	public static List<Cluster> getData2(){
+		//Clusterliste und Cluster vorbereiten
+		List<Cluster> clusterList  = new ArrayList<Cluster>();
+		Cluster c1 = new Cluster();
+		Cluster c2 = new Cluster();	
+		//xml parser anlegen
+		XMLParser parser = new XMLParser();
+		// Artikel anlegen und in eine Liste hinzufügen
+		Article a1 = parser.parse("C:/xml/6.xml");
+		Article a2 = parser.parse("C:/xml/5.xml");
+		Article a3 = parser.parse("C:/xml/4.xml");
+		Article a4 = parser.parse("C:/xml/3.xml");
+		List<Article> articles1 = new ArrayList<Article>();
+		List<Article> articles2 = new ArrayList<Article>();
+		articles1.add(a1);
+		articles1.add(a2);
+		articles2.add(a3);
+		articles2.add(a4);
+		//Cluster befüllen
+		c1.setTopArticle(parser.parse("C:/xml/2.xml"));
+		c2.setTopArticle(parser.parse("C:/xml/1.xml"));
+		c1.setArticles(articles1);
+		c2.setArticles(articles2);
+		//Cluster der Liste hinzufügen
+		clusterList.add(c1);
+		clusterList.add(c2);
+		
+		return clusterList;
+	}
 }
