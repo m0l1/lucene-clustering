31af134ca2ac9b00bca1362f922962a31f0c6428
ExceptFrequencies <johann.trautmann@gmx.de>
2015-01-10 20:03:11 +0100

    Create FileLineLister.java
    Händisch committed, toller scheiß hier.

---
+++ Lucene-Clustering/src/de/hof_university/iisys/pp_vinf12/lucene_clustering/xml/FileLineLister.java
@@ -0,0 +1,36 @@
+package de.hof_university.iisys.pp_vinf12.lucene_clustering.xml;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class FileLineLister {
+	
+	public FileLineLister(){
+		
+	}
+	
+	
+	public List<String> listXMLFilesInFile(String filePath) throws FileNotFoundException{
+		List<String> list= new ArrayList<String>();
+		File file = new File(filePath);
+		Scanner s = new Scanner(file);
+		while (s.hasNext()){
+		    list.add(s.next());
+		}
+		s.close();
+		deleteFile(file);
+		
+		return list;
+	}
+	
+	
+	// TODO Implement
+	private void deleteFile(File file){
+		
+	}
+	
+
+}
